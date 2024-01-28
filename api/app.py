37ba68a493b07d138fdaca403b79614be89ef52f
unknown <kevalsaud25@gmail.com>
2024-01-28 17:10:05 +0530

    gemini v1 vercel-deploy

--- api/app.py
+++ api/app.py
@@ -1,7 +1,7 @@
 
 from flask import Flask, request, jsonify,render_template,redirect,session
 from time import sleep
-# from LLM import *
+from LLM import *
 import google.generativeai as genai
 import os
 
@@ -27,4 +27,36 @@ def home():
     return render_template('chat.html')
 
 
+
+@app.route('/send_message', methods=['POST'])
+def get_response():
+    # user = session['user']
+    
+    user_message = request.form.get('user_message')
+    
+
+ 
+    bot_reply = generate_reply(user_message,ai_model)
+
+    response = {
+        'bot_reply': bot_reply
+    }
+    
+    return jsonify(response)
+
+
+
+
+
+def generate_reply(message,model):
+
+
+
+    query = message
+
+    response= convo(model,str(query))
+
+    AI = response[0]
+  
+    return AI
 
